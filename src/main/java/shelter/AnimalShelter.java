725709eeb07459c1d82445ad5026dbd3094f097f
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-02-12 23:16:43 -0330

    Created a enqueue method to add an animal to the queue - using instanceof to ensuredogs go to the dog queue and cats go to the cat queue

--- src/main/java/shelter/AnimalShelter.java
+++ src/main/java/shelter/AnimalShelter.java
@@ -9,4 +9,11 @@ public class AnimalShelter {
     private PriorityQueue<Dog> dogQueue = new PriorityQueue<>();
     private PriorityQueue<Cat> catQueue = new PriorityQueue<>();
 
+    public void enqueue(Animal animal) {
+        if (animal instanceof Dog) {
+            dogQueue.add((Dog) animal);
+        } else if (animal instanceof Cat) {
+            catQueue.add((Cat) animal);
+        }
+    }
 }
