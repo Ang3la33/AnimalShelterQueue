7be2f03e9948529fd48f448ea38e7d3c84305d8c
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-02-12 23:33:56 -0330

    Added methods to adopt the animal that has been in the shelter for the longest time using dequeueAny and determines whether that is a dog or a cat by comparing the dog and cat queues arrival time, also added methods if user wants specifically a dog or cat to return that queues longest resident

--- src/main/java/shelter/AnimalShelter.java
+++ src/main/java/shelter/AnimalShelter.java
@@ -9,11 +9,36 @@ public class AnimalShelter {
     private PriorityQueue<Dog> dogQueue = new PriorityQueue<>();
     private PriorityQueue<Cat> catQueue = new PriorityQueue<>();
 
+    // Method to add an animal to the shelter
     public void enqueue(Animal animal) {
         if (animal instanceof Dog) {
             dogQueue.add((Dog) animal);
         } else if (animal instanceof Cat) {
             catQueue.add((Cat) animal);
         }
+    }
+
+    // Method to adopt the oldest animal in shelter
+    public Animal dequeueAny() {
+        if (dogQueue.isEmpty() && catQueue.isEmpty()) return null;
+        if (dogQueue.isEmpty()) return dequeueCat();
+        if (catQueue.isEmpty()) return dequeueDog();
+
+        Dog oldestDog = dogQueue.peek();
+        Cat oldestCat = catQueue.peek();
+
+        // If the oldest dog arrived before the oldest cat remove and return the oldest dog
+        // Else remove and return the oldest cat
+        return (oldestDog.getArrivalTime() < oldestCat.getArrivalTime()) ? dogQueue.poll() : catQueue.poll();
+    }
+
+    // Method to adopt the dog that has been at the shelter the longest
+    public Dog dequeueDog() {
+        return dogQueue.poll();
+    }
+
+    // Method to adopt the cat that has been at the shelter the longest
+    public Cat dequeueCat() {
+        return catQueue.poll();
     }
 }
