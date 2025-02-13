25ddf3d82d05502bb7cabb5a6d123b1d0042b8bb
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-02-12 23:08:44 -0330

    Initialized the AnimalShelter class, importing models Animal, Dog, and Cat and using PriorityQueue java data structure to store elements in order with the lowest value as the highest priority (why Animal is set up with a count for arrival time ensuring that the highest priority in the queue will always be the lowest number and the number will increase as new animals are added)

---
+++ src/main/java/shelter/AnimalShelter.java
@@ -0,0 +1,12 @@
+package shelter;
+
+import models.Animal;
+import models.Dog;
+import models.Cat;
+import java.util.PriorityQueue;
+
+public class AnimalShelter {
+    private PriorityQueue<Dog> dogQueue = new PriorityQueue<>();
+    private PriorityQueue<Cat> catQueue = new PriorityQueue<>();
+
+}
