57ac1b40e5afeb0e5c7b74165a85739c40eeb1c5
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-02-12 23:00:31 -0330

    Created models Animal, Cat, and Dog. Cat and Dog extending from Animal parent.

---
+++ src/main/java/models/Animal.java
@@ -0,0 +1,27 @@
+package models;
+
+public abstract class Animal implements Comparable<Animal>{
+    private int counter = 0;
+    private int arrivalTime;
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+        this.arrivalTime = counter++;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Animal other) {
+        return Integer.compare(this.arrivalTime, other.arrivalTime);
+    }
+}
+
+
---
+++ src/main/java/models/Dog.java
@@ -0,0 +1,7 @@
+package models;
+
+public class Dog extends Animal{
+    public Dog(String name) {
+        super(name);
+    }
+}
